85700fc742cc0ba5e21b2d2630e5fed418b196a4
ricky firmansyah <rickyfirmansyah27@gmail>
2024-12-10 15:52:57 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,65 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+const RABBITMQ_API_URL = 'https://armadillo.rmq.cloudamqp.com/api/queues/gxylgqlb';
+const startTime = Date.now();
+
+const rabbitmq_check = {
+  async fetch(request, env, ctx) {
+    const uptimeInSeconds = Math.floor((Date.now() - startTime) / 1000);
+
+	const days = Math.floor(uptimeInSeconds / (3600 * 24)); 
+	const hours = Math.floor((uptimeInSeconds % (3600 * 24)) / 3600);
+	const minutes = Math.floor((uptimeInSeconds % 3600) / 60);
+	const seconds = uptimeInSeconds % 60;
+	
+    const uptimeFormatted = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+    try {
+
+      const response = await fetch(RABBITMQ_API_URL, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + btoa('gxylgqlb:OWeSHYsGOtgehoHdMHBwkbnvUOHLZQCI')
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('[CloudAMPQ] | RabbitMQ Connection failed');
+      }
+      const data = await response.json();
+      return new Response(
+        JSON.stringify({
+          status: 'success',
+          statusCode: 200,
+          message: '[CloudAMPQ] | RabbitMQ Connection established',
+          uptime: uptimeFormatted,
+          queueStatus: data,
+        }),
+        {
+          status: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+    } catch (error) {
+      console.error('[CloudAMPQ] | RabbitMQ Check Failed:', error);
+
+      return new Response(
+        JSON.stringify({
+          status: 'error',
+          statusCode: 500,
+          message: '[CloudAMPQ] | RabbitMQ Check Failed',
+          uptime: uptimeFormatted,
+        }),
+        {
+          status: 500,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+    }
+  },
 };
+
+export { rabbitmq_check as default };
