8b209975f6da15f56ba8ada571570312edc3f30e
ricky firmansyah <rickyfirmansyah27@gmail>
2024-12-10 16:09:38 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-
-const RABBITMQ_API_URL = 'https://armadillo.rmq.cloudamqp.com/api/queues/gxylgqlb';
 const startTime = Date.now();
-
 const rabbitmq_check = {
   async fetch(request, env, ctx) {
     const uptimeInSeconds = Math.floor((Date.now() - startTime) / 1000);
 
-	const days = Math.floor(uptimeInSeconds / (3600 * 24)); 
-	const hours = Math.floor((uptimeInSeconds % (3600 * 24)) / 3600);
-	const minutes = Math.floor((uptimeInSeconds % 3600) / 60);
-	const seconds = uptimeInSeconds % 60;
-	
-    const uptimeFormatted = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    const days = Math.floor(uptimeInSeconds / (3600 * 24));
+    const hours = Math.floor((uptimeInSeconds % (3600 * 24)) / 3600);
+    const minutes = Math.floor((uptimeInSeconds % 3600) / 60);
+    const seconds = uptimeInSeconds % 60;
 
+    const uptimeFormatted = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    const RABBITMQ_API_URL = env.RABBITMQ_API_URL;
     try {
 
       const response = await fetch(RABBITMQ_API_URL, {
         method: 'GET',
         headers: {
-          'Authorization': 'Basic ' + btoa('gxylgqlb:OWeSHYsGOtgehoHdMHBwkbnvUOHLZQCI')
+          'Authorization': 'Basic ' + btoa(env.AUTH_RABBITMQ),
         }
       });
 
